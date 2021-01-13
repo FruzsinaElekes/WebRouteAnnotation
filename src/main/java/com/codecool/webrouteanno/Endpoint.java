6c7b35ac4adea7245a27d5c2da868763d5a4c5fc
FruzsinaElekes <elekes.fr@gmail.com>
2021-01-13 20:06:04 +0100

    Add parametrized /profile/<s> endpoint

--- src/main/java/com/codecool/webrouteanno/Endpoint.java
+++ src/main/java/com/codecool/webrouteanno/Endpoint.java
@@ -3,22 +3,27 @@ package com.codecool.webrouteanno;
 public class Endpoint {
 
     @WebRoute()
-    String handleIndexEndpoint(){
+    public String getIndex(){
         return "Index";
     }
 
     @WebRoute(path="/test")
-    String getTestEndpoint(){
+    public String getTest(){
         return "This is the get test route";
     }
 
     @WebRoute(path="/test", method="POST")
-    String postTestEndpoint(){
+    public String postTest(){
         return "This is the post test route";
     }
 
     @WebRoute(path="/profile")
-    String handleProfileEndpoint(){
+    public String getProfile(){
         return "this is the profile endpoint";
+    }
+
+    @WebRoute(path="/profile/<s>")
+    public String getProfileForUser(String userName){
+        return String.format("this is the profile endpoint for %s", userName);
     }
 }
